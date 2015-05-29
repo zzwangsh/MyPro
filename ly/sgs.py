160c5389919e773216fb7980cfefe1beddd31aae
zzwangsh <zzwangsh@163.com>
2015-05-29 09:35:39 +0800

    历史性进步

--- ly/sgs.py
+++ ly/sgs.py
@@ -1,60 +1,80 @@
-#/usr/local/bin/env python
+
 #coding:utf-8
 
 import urllib
 import urllib2
 import cookielib
 import re
+#import sys 
+#reload(sys) 
+#sys.setdefaultencoding('utf-8')
 
-
-class SGS:
+class searchInfo:
     def __init__(self):
-        self.loginUrl = 'http://www.sgs.gov.cn/lz/etpsInfo.do?method=doSearch'
-        self.gradeUrl = 'http://www.sgs.gov.cn/lz/etpsInfo.do?method=viewDetail'
-        self.cookies = cookielib.CookieJar()  
-        self.httpHandler = urllib2.HTTPHandler(debuglevel=1)
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers = { 'User-Agent' : self.user_agent }
+        self.initUrl = 'http://www.36yc.com'
+        self.searchUrl = 'http://www.36yc.com/search'
+        #self.viewUrl = 'http://ww.36yc.com/%s +'+ % str(viewDetailUrl)
+        self.cookies = cookielib.CookieJar()
         self.postdata = urllib.urlencode({
-            'searchType':'1',
-            'keyWords':'上海来伊份电子商务有限公司'
-          })
+            'area':'shanghai',
+            'key':'上海来伊份电子商务有限公司',
+            'search':'查企业',
+            't':'ent_name'
+            })
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
 
     def getPage(self):
         request = urllib2.Request(
-            url = self.loginUrl,
-            data = self.postdata)
+            url = self.searchUrl,
+            data = self.postdata,
+            headers = self.headers)
         result = self.opener.open(request)
         contents = result.read()
-        return contents
+        #return contents.encode('utf8')
+        unicodePage = contents.decode("utf-8")
+        return unicodePage
 
-    def getEspsid(self):
+
+    def getlink(self):
         page = self.getPage()
-        items = re.findall(u'<a href=.*?viewDetail\(\'(\d+)\'\).*?>',page,re.S)
-        if items:
-            espsid = items[0]
-            return espsid
-            print type(espsid)
-        else:
-            print "Not Found"
+       # print page
+        pattern = re.findall('<tr>.*?<tr>.*?<td>(.*?)<\/td>.*?<td>.*?<\/td><td>.*?<\/td><td><a href="(.*?)".*?target=".*?',page,re.S)
+        items = []
+        #print pattern
+        #for item in pattern:
+        #    items.append(item[0].encode('utf-8'))
+        #print items[0]
+        for item in pattern:
+            items.append(item[1])
+        return items[0]
+        
+        
+        #print items
+        #companyName = items[0]
+        #companyLink = items.groups(2)
+        #print companyName.encode
+        #print companyLink.encode
+
+    def getViewPage(self):
+        viewLink = self.getlink()
+        viewUrl = self.initUrl + viewLink
+        viewRequest = urllib2.Request(url = viewUrl,headers = self.headers)
+        viewResult = self.opener.open(viewRequest)
+        viewContents = viewResult.read()
+        viewUnicodePage = viewContents.decode("utf-8")
+        return viewUnicodePage
 
     def getResult(self):
-        postid = int(self.getEspsid())
-        newpostdata = urllib.urlencode({'espId':postid})
-        
-        headers = {
-            'Referer':'http://www.sgs.gov.cn/lz/etpsInfo.do?method=doSearch',
-            'Cache-Control':'max-age=0'
-        }    
-        print headers
-        newRequest = urllib2.Request(url=self.gradeUrl,data=newpostdata,headers=headers)
-        #    url = self.gradeUrl,
-        #   data = newpostdata)
-        result = self.opener.open(newRequest)
-        contents = result.read()
-        print contents
-
-        
+        viewPage = self.getViewPage()
+        fucker = re.findall('<td.*?class=".*?width=.*?><strong>(.*?)<\/strong><\/td>',viewPage,re.S)
+        print fucker[2].encode('utf-8')
+        #result = []
+        #for item in fucker:
+        #    result.append(item[0].encode('utf-8'))
+        #print result[0]
 
                
-sgs = SGS()
+sgs = searchInfo()
 sgs.getResult()
